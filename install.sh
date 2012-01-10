879876c80ce75103442169b10754affa94e80235
lxyu <lx.yu@ele.me>
2012-01-10 19:02:08 +0800

    add confirm before install to /root
    add some echo lines to show current progress.

--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@
 
 # remove existing files
 cd ~ || exit 1
+echo "Removing existing dot files ..."
 rm -r ~/dotfiles
 rm -f .bash_aliases .bashrc .profile .tmux.conf .rtorrent.rc .inputrc .htoprc .gitconfig .irssi
 
+echo "Cloning new dot files ..."
 git clone git://github.com/lxyu/dotfiles.git || exit 1
 
 # link to dotfiles
+echo "Installing dot files ..."
 ln -s dotfiles/.irssi
 ln -s dotfiles/.bash_aliases
 ln -s dotfiles/.bashrc
@@ -18,6 +21,13 @@ ln -s dotfiles/.profile
 ln -s dotfiles/.rtorrent.rc
 ln -s dotfiles/.tmux.conf
 
+read -p "Do you want to link dot files for root?" choice
+case "$choice" in
+    y|Y ) ;;
+    * ) exit;;
+esac
+
+echo "Linking dot files for root ..."
 sudo mkdir -p /root
 sudo rm -f /root/.bash_aliases /root/.bashrc /root/.profile /root/.tmux.conf /root/.vim /root/.vimrc
 sudo ln -s ~/.bashrc /root/.bashrc
