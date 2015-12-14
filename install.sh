b4eb4e2d8a77325180365e9d5f9ccda4335a73d9
Lixin Yu <i@lxyu.net>
2015-12-14 22:33:36 +0800

    add xresource

--- install.sh
+++ install.sh
@@ -9,37 +9,43 @@ echo "Cloning new dot files ..."
 /usr/bin/env git clone git://github.com/lxyu/dotfiles.git ~/.dotfiles || exit 1
 
 # remove old existing files
+rm -vf ~/.Xresources
 rm -vf ~/.bash_aliases
 rm -vf ~/.bash_prompt
 rm -vf ~/.bashrc
 rm -vf ~/.gitconfig
 rm -vf ~/.htoprc
+rm -vf ~/.i3blocks.conf
+rm -vf ~/.i3status.conf
+rm -vf ~/.ideavimrc
 rm -vf ~/.inputrc
 rm -vf ~/.profile
 rm -vf ~/.rtorrent.rc
 rm -vf ~/.tmux.conf
+rm -vf ~/.xbindkeysrc
+rm -vf ~/.xinitrc
+rm -vrf ~/.i3/
 rm -vrf ~/.irssi
+rm -vrf ~/.pentadactyl/
 rm -vrf ~/.pip
 rm -vrf ~/.tmux-powerlinerc
 rm -vrf ~/.virtualenvs
 
-rm -vf ~/.i3blocks.conf
-rm -vf ~/.i3status.conf
-rm -vf ~/.ideavimrc
-rm -vf ~/.xbindkeysrc
-rm -vf ~/.xinitrc
-rm -vrf ~/.i3/
-rm -vrf ~/.pentadactyl/
-
 # link to dotfiles
 echo "Installing dot files ..."
+ln -vs ~/.dotfiles/.Xresources ~/.Xresources
 ln -vs ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -vs ~/.dotfiles/.bash_prompt ~/.bash_prompt
 ln -vs ~/.dotfiles/.bashrc ~/.bashrc
 ln -vs ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -vs ~/.dotfiles/.htoprc ~/.htoprc
+ln -vs ~/.dotfiles/.i3 ~/.i3
+ln -vs ~/.dotfiles/.i3blocks.conf ~/.i3blocks.conf
+ln -vs ~/.dotfiles/.i3status.conf ~/.i3status.conf
+ln -vs ~/.dotfiles/.ideavimrc ~/.ideavimrc
 ln -vs ~/.dotfiles/.inputrc ~/.inputrc
 ln -vs ~/.dotfiles/.irssi ~/.irssi
+ln -vs ~/.dotfiles/.pentadactyl/.pentadactylrc ~/.pentadactylrc
 ln -vs ~/.dotfiles/.pip ~/.pip
 ln -vs ~/.dotfiles/.profile ~/.profile
 ln -vs ~/.dotfiles/.rtorrent.rc ~/.rtorrent.rc
@@ -47,12 +53,7 @@ ln -vs ~/.dotfiles/.tmux-osx.conf ~/.tmux-osx.conf
 ln -vs ~/.dotfiles/.tmux-powerlinerc ~/.tmux-powerlinerc
 ln -vs ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -vs ~/.dotfiles/.virtualenvs ~/.virtualenvs
-ln -vs ~/.dotfiles/.i3blocks.conf ~/.i3blocks.conf
-ln -vs ~/.dotfiles/.i3status.conf ~/.i3status.conf
-ln -vs ~/.dotfiles/.ideavimrc ~/.ideavimrc
 ln -vs ~/.dotfiles/.xbindkeysrc ~/.xbindkeysrc
 ln -vs ~/.dotfiles/.xinitrc ~/.xinitrc
-ln -vs ~/.dotfiles/.i3 ~/.i3
-ln -vs ~/.dotfiles/.pentadactyl/.pentadactylrc ~/.pentadactylrc
 
 cd ~/.dotfiles/ && git submodule update --init
