f0b4f2213f4c5f7f5d43b3d6b1514934fa02ea02
Lixin Yu <i@lxyu.net>
2016-03-28 14:59:54 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,59 +1,46 @@
 #!/bin/bash
 
 # remove existing files
-echo "Removing existing dot files ..."
-rm -rf ~/.dotfiles
-rm -rf ~/.autoenv
-
 echo "Cloning new dot files ..."
 /usr/bin/env git clone git://github.com/lxyu/dotfiles.git ~/.dotfiles || exit 1
 
-# remove old existing files
-rm -vf ~/.Xresources
-rm -vf ~/.bash_aliases
-rm -vf ~/.bash_prompt
-rm -vf ~/.bashrc
-rm -vf ~/.gitconfig
-rm -vf ~/.htoprc
-rm -vf ~/.i3blocks.conf
-rm -vf ~/.i3status.conf
-rm -vf ~/.ideavimrc
-rm -vf ~/.inputrc
-rm -vf ~/.profile
-rm -vf ~/.rtorrent.rc
-rm -vf ~/.tmux.conf
-rm -vf ~/.xbindkeysrc
-rm -vf ~/.xinitrc
-rm -vrf ~/.i3/
-rm -vrf ~/.irssi
-rm -vrf ~/.pentadactyl/
-rm -vrf ~/.pip
-rm -vrf ~/.tmux-powerlinerc
-rm -vrf ~/.virtualenvs
+echo "Linking..."
+files=(
+    # files
+    .Xmodmap \
+    .Xresources \
+    .aria2 \
+    .bash_aliases \
+    .bash_prompt \
+    .bashrc \
+    .gitconfig \
+    .htoprc \
+    .i3blocks.conf \
+    .i3status.conf \
+    .ideavimrc \
+    .inputrc \
+    .pentadactylrc \
+    .profile \
+    .rtorrent.rc \
+    .stylish-haskell.yaml \
+    .tmux-osx.conf \
+    .tmux-powerlinerc \
+    .tmux.conf \
+    .xbindkeysrc \
+    .xinitrc \
+    .zprofile \
+    .zshrc \
 
-# link to dotfiles
-echo "Installing dot files ..."
-ln -vs ~/.dotfiles/.Xresources ~/.Xresources
-ln -vs ~/.dotfiles/.bash_aliases ~/.bash_aliases
-ln -vs ~/.dotfiles/.bash_prompt ~/.bash_prompt
-ln -vs ~/.dotfiles/.bashrc ~/.bashrc
-ln -vs ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -vs ~/.dotfiles/.htoprc ~/.htoprc
-ln -vs ~/.dotfiles/.i3 ~/.i3
-ln -vs ~/.dotfiles/.i3blocks.conf ~/.i3blocks.conf
-ln -vs ~/.dotfiles/.i3status.conf ~/.i3status.conf
-ln -vs ~/.dotfiles/.ideavimrc ~/.ideavimrc
-ln -vs ~/.dotfiles/.inputrc ~/.inputrc
-ln -vs ~/.dotfiles/.irssi ~/.irssi
-ln -vs ~/.dotfiles/.pentadactyl/.pentadactylrc ~/.pentadactylrc
-ln -vs ~/.dotfiles/.pip ~/.pip
-ln -vs ~/.dotfiles/.profile ~/.profile
-ln -vs ~/.dotfiles/.rtorrent.rc ~/.rtorrent.rc
-ln -vs ~/.dotfiles/.tmux-osx.conf ~/.tmux-osx.conf
-ln -vs ~/.dotfiles/.tmux-powerlinerc ~/.tmux-powerlinerc
-ln -vs ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -vs ~/.dotfiles/.virtualenvs ~/.virtualenvs
-ln -vs ~/.dotfiles/.xbindkeysrc ~/.xbindkeysrc
-ln -vs ~/.dotfiles/.xinitrc ~/.xinitrc
+    # dirs
+    .i3 \
+    .irssi \
+    .pentadactyl \
+    .tmux-themes \
+    .pip \
+    .virtualenvs \
+    .zsh
+)
 
-cd ~/.dotfiles/ && git submodule update --init
+for f in $files ; do
+    ln -vsfn ~/.dotfiles/$f ~/$f
+done
